6cb0763347524ef9d559f6eceabffa68949d91e3
jjacque1 <jacksonjacque.jj@gmail.com>
2024-01-08 21:52:11 -0500

    react crash course

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Todo from './components/Todo.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>My Todo List</h1>
+      <Todo></Todo>
+      <Todo></Todo>
+      <Todo></Todo>
+    </div>
+  );
+}
+
+export default App;
