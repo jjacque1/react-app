a6952eb914f321d831e78cb8a64ecd9f3012e75e
jjacque1 <jacksonjacque.jj@gmail.com>
2024-01-14 18:52:13 -0500

    Event Handlers => started

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
-import Modal from './components/Modal.jsx'
+import Modal from "./components/Modal.jsx";
 
 function App() {
   return (
     <div>
       <Title></Title>
+      <div>
+        <input type="text" onChange={(event) => {
+          console.log(event.target.value)
+        }}/>
+        <button>Add todo</button>
+      </div>
       <div className="todo__wrapper">
-        <Todo title="Finish Frontend Simplified"
-              paragraph="Code along with Frontend Simplified step by step"></Todo>
-        <Todo title="Finish Interview Section"
-              paragraph="Finish every interview question in the next 3 months"></Todo>
-        <Todo title="Landa $100k Job"
-              paragraph="Apply to 100 Jobs"></Todo>
+        <Todo title="Finish Frontend Simplified"></Todo>
+        <Todo title="Finish Interview Section"></Todo>
+        <Todo title="Land a $100k Job"></Todo>
       </div>
 
       <Modal title="Areyou sure you want to delete?">
 
       </Modal>
-      <Modal title="Are you sure you want to add?">
-              
-      </Modal>
+      
     </div>
   );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,15 @@
-import './Todo.css'
+import "./Todo.css";
 
-function Todo({title, paragraph}) {
-  console.log(title)
+function Todo({ title }) {
+  function deleteTodo(id) {
+    console.log("deleteTodo()", title);
+  }
+
   return (
-    <div className='todo'>
-      <h2>{title}</h2>
-      <p>{paragraph}</p>
-      <button>Delete</button>
+    <div className="todo">
+      <p>{title}</p>
+
+      <button onClick={() => deleteTodo(1)}>Delete</button>
     </div>
   );
 }
