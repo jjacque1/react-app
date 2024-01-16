d983e3bf1fcda3ae41545e13dc951eddf277fa89
jjacque1 <jacksonjacque.jj@gmail.com>
2024-01-15 22:04:05 -0500

    React Hooks => started

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
 import Modal from "./components/Modal.jsx";
+import React, { useState } from "react";
 
 function App() {
+  let isModalOpen = false;
+
+  function toggleModal() {
+    isModalOpen = !isModalOpen
+    console.log(isModalOpen)
+  }
+
   return (
     <div>
       <Title></Title>
       <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }}/>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
         <button>Add todo</button>
       </div>
       <div className="todo__wrapper">
@@ -19,10 +30,7 @@ function App() {
         <Todo title="Land a $100k Job"></Todo>
       </div>
 
-      <Modal title="Areyou sure you want to delete?">
-
-      </Modal>
-      
+      {isModalOpen ? <Modal title="Confirm delete?"></Modal>: null}
     </div>
   );
 }
