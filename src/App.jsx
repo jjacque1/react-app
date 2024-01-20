504ae216a000643b1d27ad319275a020b5837969
jjacque1 <jacksonjacque.jj@gmail.com>
2024-01-19 21:39:19 -0500

    React Hooks => using objects in 'useState'

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import React, { useState } from "react";
 
 
 function App() {
-  // return <Counter/> 
+  return <Counter/> 
 
   const [showModal, setShowModal] = useState(false);
   return (
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,35 @@
-/**
- * 1. Create a "Counter.jsx" component
- * 2. Create a default `count` of 0
- * 3. Create a button to increment 'count' by 1
- * 4. Create a button to decrement 'count' by 1
- * 5. Import your Counter in App.jsx and test it
- */
-
+import React, { useState } from "react";
 
 function Counter() {
-    return (
-        <div>
-        <button></button>
-        {0}
-        <button></button>
+  const [counter, setCounter] = useState(0);
+  function incrementCounter() {
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+  }
+
+  function decrementCounter() {
+    setCounter((prevCounter) => prevCounter - 1)
+    setCounter((prevCounter) => prevCounter - 1);
+  }
+
+  return (
+    <div>
+      <button onClick={decrementCounter}>-</button>
+      {counter}
+      <button onClick={incrementCounter}>+</button>
     </div>
-    )
-    
+  );
 }
 
-export default Counter
+export default Counter;
+
+
+
+
+// /**
+//  * 1. Create a "Counter.jsx" component
+//  * 2. Create a default `count` of 0
+//  * 3. Create a button to increment 'count' by 1
+//  * 4. Create a button to decrement 'count' by 1
+//  * 5. Import your Counter in App.jsx and test it
+//  */
