3e011a26fa50c8acb180377ce8ad3a28e8d42d2d
jjacque1 <jacksonjacque.jj@gmail.com>
2024-02-04 22:32:07 -0500

    Routing => continues

--- src/App.jsx
+++ src/App.jsx
@@ -5,66 +5,73 @@ import Modal from "./components/Modal.jsx";
 // import Counter from "./components/Counter.jsx";
 import React, { useState, useEffect } from "react";
 
+// function App() {
+//   const [showModal, setShowModal] = useState(false);
+
+//   function onTodoDelete() {
+//     setShowModal(true);
+//   }
+
+//   function cancleModal() {
+//     setShowModal(false);
+//   }
+
+//   function confirmModal() {
+//     setShowModal(false);
+//   }
+
+//   // useEffect(() => {
+//   //   console.log('on mount') /* This Code runs ony on mount */
+//   // }, [])
+
+//   // useEffect(() => {
+//   //   console.log(`on mount AND on ${showModal} change`) /* This Code runs ony on mount AND on showModal change because of the dependency [showModal] passed*/
+//   // }, [showModal])
+
+//   useEffect(() => {
+//     console.log("EVERY render") 
+//   })
+
+//   return (
+//     <div>
+//       <Title></Title>
+//       <div>
+//         <input
+//           type="text"
+//           onChange={(event) => {
+//             console.log(event.target.value);
+//           }}
+//         />
+//         <button onClick={() => setShowModal(true)}>Add todo</button>
+//       </div>
+//       <div className="todo__wrapper">
+//         <Todo
+//           onTodoDelete={onTodoDelete}
+//           title="Finish Frontend Simplified"
+//         ></Todo>
+//         <Todo
+//           onTodoDelete={onTodoDelete}
+//           title="Finish Interview Section"
+//         ></Todo>
+//         <Todo onTodoDelete={onTodoDelete} title="Land a $100k Job"></Todo>
+//       </div>
+
+//       {showModal && (
+//         <Modal
+//           cancleModal={cancleModal}
+//           confirmModal={confirmModal}
+//           title="Confirm delete?"
+//         ></Modal>
+//       )}
+//     </div>
+//   );
+// }
+
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
-  function onTodoDelete() {
-    setShowModal(true);
-  }
-
-  function cancleModal() {
-    setShowModal(false);
-  }
-
-  function confirmModal() {
-    setShowModal(false);
-  }
-
-  // useEffect(() => {
-  //   console.log('on mount') /* This Code runs ony on mount */
-  // }, [])
-
-  // useEffect(() => {
-  //   console.log(`on mount AND on ${showModal} change`) /* This Code runs ony on mount AND on showModal change because of the dependency [showModal] passed*/
-  // }, [showModal])
-
-  useEffect(() => {
-    console.log("EVERY render") 
-  })
-
   return (
-    <div>
-      <Title></Title>
-      <div>
-        <input
-          type="text"
-          onChange={(event) => {
-            console.log(event.target.value);
-          }}
-        />
-        <button onClick={() => setShowModal(true)}>Add todo</button>
-      </div>
-      <div className="todo__wrapper">
-        <Todo
-          onTodoDelete={onTodoDelete}
-          title="Finish Frontend Simplified"
-        ></Todo>
-        <Todo
-          onTodoDelete={onTodoDelete}
-          title="Finish Interview Section"
-        ></Todo>
-        <Todo onTodoDelete={onTodoDelete} title="Land a $100k Job"></Todo>
-      </div>
-
-      {showModal && (
-        <Modal
-          cancleModal={cancleModal}
-          confirmModal={confirmModal}
-          title="Confirm delete?"
-        ></Modal>
-      )}
-    </div>
-  );
+    <div></div>
+  )
 }
+
 
 export default App;
